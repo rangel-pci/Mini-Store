2073d4880dad010407fbc77e04af435e6d4cc0ac
Rangel Pereira <rangel.pci@gmail.com>
2023-05-02 18:05:05 -0300

    PWA ready

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -13,5 +17,5 @@ export default defineConfig({
   },
   server: {
     host: true,
-  }
+  },
 })
